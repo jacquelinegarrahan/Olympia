6b0f86e493c1498c7155904ebf4692cde3789d7a
Jacqueline Garrahan <jacquelinegarrahan@Jacquelines-MacBook-Pro.local>
2019-05-13 12:04:51 -0400

    dur

--- djnn/train/duration.py
+++ djnn/train/duration.py
@@ -68,6 +68,7 @@ def prepare_durations(progression, mapping, sequence_len):
 def diversity_check(duration):
 	#use the index of dispersion here as a filter
 	iod = numpy.var(duration)/numpy.mean(duration)
+	print(iod)
 	if iod > 1:
 		return True
 	else:
