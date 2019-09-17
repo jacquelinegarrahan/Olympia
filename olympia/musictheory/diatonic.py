7f79bc9c4f581962f6ce7059a56c0ee458df5895
matthew <m.r.l.2784@gmail.com>
2019-09-17 13:27:09 -0700

    diatonic triads

--- olympia/musictheory/diatonic.py
+++ olympia/musictheory/diatonic.py
@@ -1,66 +1,71 @@
-#DIATONIC TRIADS
 
-note = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
+note = range(100)
 
-
+#major diatonic triads
 def major_diatonic_triads(key):
     major_diatonic = []
-    #I
     major_diatonic.append([note[key], note[key + 4], note[key + 7]])
-    #ii
     major_diatonic.append([note[key + 2], (note[key + 2] + 3), (note[key + 2] + 7)])
-    #iii
     major_diatonic.append([note[key + 4], (note[key + 4] + 3), (note[key + 4] + 7)])
-    #IV
     major_diatonic.append([note[key + 5], (note[key + 5] + 4), (note[key + 5] + 7)])
-    #V
     major_diatonic.append([note[key + 7], (note[key + 7] + 4), (note[key + 7] + 7)])
-    #vi
     major_diatonic.append([note[key + 9], (note[key + 9] + 3), (note[key + 9] + 7)])
-    #vii
     major_diatonic.append([note[key + 11], (note[key + 11] + 3), (note[key + 11] + 6)])
-    #I2
     major_diatonic.append([note[key + 12], (note[key + 12] + 3), (note[key + 12] + 7)])
-
     return major_diatonic
 
-C_Major_Diatonic = major_diatonic_triads(0)
-Cs_Major_Diatonic = major_diatonic_triads(1)
-D_Major_Diatonic = major_diatonic_triads(2)
-Ds_Major_Diatonic = major_diatonic_triads(3)
-print(C_Major_Diatonic)
+c_maj_triads = major_diatonic_triads(0)
+c_sharp_maj_triads = major_diatonic_triads(1)
+d_flat_maj_triads = major_diatonic_triads(1)
+d_maj_triads = major_diatonic_triads(2)
+d_sharp_maj_triads = major_diatonic_triads(3)
+e_flat_maj_triads = major_diatonic_triads(3)
+e_maj_triads = major_diatonic_triads(4)
+f_maj_triads = major_diatonic_triads(5)
+f_sharp_maj_triads = major_diatonic_triads(6)
+g_flat_maj_triads = major_diatonic_triads(6)
+g_maj_triads = major_diatonic_triads(7)
+g_sharp_maj_triads = major_diatonic_triads(8)
+a_flat_maj_triads = major_diatonic_triads(8)
+a_maj_triads = major_diatonic_triads(9)
+a_sharp_maj_triads = major_diatonic_triads(10)
+b_flat_maj_triads = major_diatonic_triads(10)
+b_maj_triads = major_diatonic_triads(11)
 
-#prints [[0,4,7],[2,5,9], [4,7,11], [5,9,12], [7,11,14], [9,12,16], [11,14,17], [12,15,19]]
-#          I       ii       iii       IV          V         vi         vii           I2
 
 
-
+#natural minor diatonic triads
 def minor_diatonic_triads(key):
     minor_diatonic = []
-    #I
-    minor_diatonic.append([note[key], note[key + 4], note[key + 7]])
-    #ii
-    minor_diatonic.append([note[key + 2], (note[key + 2] + 3), (note[key + 2] + 7)])
-    #iii
-    minor_diatonic.append([note[key + 3], (note[key + 3] + 3), (note[key + 3] + 6)])
-    #IV
-    minor_diatonic.append([note[key + 5], (note[key + 5] + 4), (note[key + 5] + 7)])
-    #V
+    minor_diatonic.append([note[key], note[key + 3], note[key + 7]])
+    minor_diatonic.append([note[key + 2], (note[key + 2] + 3), (note[key + 2] + 6)])
+    minor_diatonic.append([note[key + 3], (note[key + 3] + 4), (note[key + 3] + 7)])
+    minor_diatonic.append([note[key + 5], (note[key + 5] + 3), (note[key + 5] + 7)])
     minor_diatonic.append([note[key + 7], (note[key + 7] + 3), (note[key + 7] + 7)])
-    #vi
-    minor_diatonic.append([note[key + 8], (note[key + 8] + 3), (note[key + 8] + 7)])
-    #vii
+    minor_diatonic.append([note[key + 8], (note[key + 8] + 4), (note[key + 8] + 7)])
     minor_diatonic.append([note[key + 10], (note[key + 10] + 4), (note[key + 10] + 7)])
-    #I2
     minor_diatonic.append([note[key + 12], (note[key + 12] + 3), (note[key + 12] + 7)])
-
     return minor_diatonic
 
-C_Minor_Diatonic = minor_diatonic_triads(0)
-Cs_Minor_Diatonic = minor_diatonic_triads(1)
+c_min_triads = minor_diatonic_triads(0)
+c_sharp_min_triads = minor_diatonic_triads(1)
+d_flat_min_triads = minor_diatonic_triads(1)
+d_min_triads = minor_diatonic_triads(2)
+d_sharp_min_triads = minor_diatonic_triads(3)
+e_flat_min_triads = minor_diatonic_triads(3)
+e_min_triads = minor_diatonic_triads(4)
+f_min_triads = minor_diatonic_triads(5)
+f_sharp_min_triads = minor_diatonic_triads(6)
+g_flat_min_triads = minor_diatonic_triads(6)
+g_min_triads = minor_diatonic_triads(7)
+g_sharp_min_triads = minor_diatonic_triads(8)
+a_flat_min_triads = minor_diatonic_triads(8)
+a_min_triads = minor_diatonic_triads(9)
+a_sharp_min_triads = minor_diatonic_triads(10)
+b_flat_min_triads = minor_diatonic_triads(10)
+b_min_triads = minor_diatonic_triads(11)
 
-print(C_Minor_Diatonic)
-
-
-#prints[[0,4,7], [2,5,9], [3,6,9], [5,9,12], [7,10,14], [8,11,15], [10,14,17], [12,15,19]]
-#          I        ii       iii      VI           V         vi        vii         I2
+#update, i had written out a few
+#of the intervals wrong. still
+#having trouble getting the octave
+#part of it though
