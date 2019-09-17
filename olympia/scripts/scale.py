67c35ce9fb3b79cf0096c45d8ba105dd28fa7ab2
matthew <m.r.l.2784@gmail.com>
2019-09-17 02:54:17 -0700

    scale theory

--- olympia/scripts/scale.py
+++ olympia/scripts/scale.py
@@ -1,8 +1,6 @@
+note = range(100)
 
-#start with the note values represented as numbers
-note = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
-#       C  Cs D  Ds E  F  Fs G  Gs A  As  B   C   Cs2  D2 Ds2 E2  F2  Fs2 G2  Gs2 A2  As2 B2  C3
-
+#major scale
 def major_scale(key):
     major_key = []
     major_key.append(note[key])
@@ -13,14 +11,43 @@ def major_scale(key):
     major_key.append(note[key + 9])
     major_key.append(note[key + 11])
     major_key.append(note[key + 12])
-    return major_key
-#using the major scale intervals to create new list with only notes within that scale
+    octave2 = [knote + 12 for knote in major_key]
+    octave3 = [knote + 12 for knote in octave2]
+    octave4 = [knote + 12 for knote in octave3]
+    octave5 = [knote + 12 for knote in octave4]
+    octave6 = [knote + 12 for knote in octave5]
+    octave7 = [knote + 12 for knote in octave6]
+    octave8 = [knote + 12 for knote in octave7]
+    octaves_sum = major_key + octave2 + octave3 + octave4 + octave5 + octave6 + octave7 + octave8
+    from itertools import groupby
+    all_octaves = [i[0] for i in groupby(octaves_sum)]
+
+    return all_octaves
+
+
 c_major = major_scale(0)
-print(c_major)
-#prints [0, 2, 4, 5, 7, 9, 11, 12]
-#aka:   [C, D, E, F, G, A, B, C] which is the CMajor scale
+c_sharp_major = major_scale(1)
+d_flat_major = major_scale(1)
+d_major = major_scale(2)
+d_sharp_major = major_scale(3)
+e_flat_major = major_scale(3)
+e_major = major_scale(4)
+f_major = major_scale(5)
+f_sharp_major = major_scale(6)
+g_flat_major = major_scale(6)
+g_major = major_scale(7)
+g_sharp_major = major_scale(8)
+a_flat_major = major_scale(8)
+a_major = major_scale(9)
+a_sharp_major = major_scale(10)
+b_flat_major = major_scale(10)
+b_major = major_scale(11)
 
-#heres the same thing but using the minor scale intervals
+#the sharps and flats that are adjacent are
+#really just the same note that has two names
+
+
+#natural minor scale
 def minor_scale(key):
     minor_key = []
     minor_key.append(note[key])
@@ -31,66 +58,32 @@ def minor_scale(key):
     minor_key.append(note[key + 8])
     minor_key.append(note[key + 10])
     minor_key.append(note[key + 12])
-    return minor_key
+    octave2 = [knote + 12 for knote in minor_key]
+    octave3 = [knote + 12 for knote in octave2]
+    octave4 = [knote + 12 for knote in octave3]
+    octave5 = [knote + 12 for knote in octave4]
+    octave6 = [knote + 12 for knote in octave5]
+    octave7 = [knote + 12 for knote in octave6]
+    octave8 = [knote + 12 for knote in octave7]
+    octaves_sum = minor_key + octave2 + octave3 + octave4 + octave5 + octave6 + octave7 + octave8
+    from itertools import groupby
+    all_octaves = [i[0] for i in groupby(octaves_sum)]
+    return all_octaves
+
 c_minor = minor_scale(0)
-print(c_minor)
-#prints [0, 2, 3, 5, 7, 8, 10, 12]
-#aka:   [C, D, D#, F, G, G#, A#, C] which is the CMinor scale
-
-
-
-
-
-#OCTAVES
-
-#could even use math to reach the whole span of octaves you have available
-
-note = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
-#       C  Cs D  Ds E  F  Fs G  Gs A  As  B   C   Cs2  D2 Ds2 E2  F2  Fs2 G2  Gs2 A2  As2 B2  C3
-
-def major_scale(key):
-    major_key = []
-    major_key.append(note[key])
-    major_key.append(note[key + 2])
-    major_key.append(note[key + 4])
-    major_key.append(note[key + 5])
-    major_key.append(note[key + 7])
-    major_key.append(note[key + 9])
-    major_key.append(note[key + 11])
-    major_key.append(note[key + 12])
-    major_key_octave2 = [knote + 12 for knote in major_key]
-    major_key_octave3 = [knote + 12 for knote in major_key_octave2]
-    all_octaves = major_key + major_key_octave2 + major_key_octave3
-    return all_octaves
-
-c_major_scale_3octaves = major_scale(0)
-print(c_major_scale_3octaves)
-
-note = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
-#       C  Cs D  Ds E  F  Fs G  Gs A  As  B   C   Cs2  D2 Ds2 E2  F2  Fs2 G2  Gs2 A2  As2 B2  C3
-
-
-def minor_scale(key):
-    minor_key = []
-    minor_key.append(note[key])
-    minor_key.append(note[key + 2])
-    minor_key.append(note[key + 3])
-    minor_key.append(note[key + 5])
-    minor_key.append(note[key + 7])
-    minor_key.append(note[key + 8])
-    minor_key.append(note[key + 10])
-    minor_key.append(note[key + 12])
-    minor_key_octave2 = [knote + 12 for knote in minor_key]
-    minor_key_octave3 = [knote + 12 for knote in minor_key_octave2]
-    all_octaves = minor_key + minor_key_octave2 + minor_key_octave3
-    return all_octaves
-
-c_minor_scale_3octaves = minor_scale(0)
-print(c_minor_scale_3octaves)
-
-# prints [0, 2, 3, 5, 7, 8, 10, 12, 12, 14, 15, 17, 19, 20, 22, 24, 24, 26, 27, 29, 31, 32, 34, 36]
-# this is just the C Minor scale but now spanning 3 octaves
-
-
-
-#random melody generator
+c_sharp_minor = minor_scale(1)
+d_flat_minor = minor_scale(1)
+d_minor = minor_scale(2)
+d_sharp_minor = minor_scale(3)
+e_flat_minor = minor_scale(3)
+e_minor = minor_scale(4)
+f_minor= minor_scale(5)
+f_sharp_minor = minor_scale(6)
+g_flat_minor = minor_scale(6)
+g_minor = minor_scale(7)
+g_sharp_minor = minor_scale(8)
+a_flat_minor = minor_scale(8)
+a_minor = minor_scale(9)
+a_sharp_minor = minor_scale(10)
+b_flat_minor = minor_scale(10)
+b_minor = minor_scale(11)
